6e2fa6e2ca6f91e01863f73ecfaa268448fe4ba5
benhern <43895569+benhern@users.noreply.github.com>
2024-01-08 01:08:47 -0600

    Added DarkMode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,23 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/Components/Header'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/Components/Header";
+import Providers from "./Providers";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Imdb Clone',
-  description: 'This is a movie database clone',
-}
+  title: "Imdb Clone",
+  description: "This is a movie database clone",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header/>{children}</body>
+        <Providers>
+          <Header/>
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
